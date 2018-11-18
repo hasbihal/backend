bdac8684472610246f9e281b89934aeeb32d057b
Murat Bastas <muratbsts@gmail.com>
2018-11-18 23:21:35 +0300

    Aws S3 URL schema definition

--- config/prod.exs
+++ config/prod.exs
@@ -81,8 +81,12 @@ config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   max_attempts: 5,
-  region: System.get_env("AWS_REGION")
-
+  region: System.get_env("AWS_REGION"),
+  s3: [
+    scheme: "https://",
+    host: "s3." <> System.get_env("AWS_REGION") <> ".amazonaws.com",
+    region: System.get_env("AWS_REGION")
+  ]
 config :arc,
   storage: Arc.Storage.S3,
   bucket: {:system, "AWS_S3_BUCKET"}
